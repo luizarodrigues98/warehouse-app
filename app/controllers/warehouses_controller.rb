f2bc47ab77144134c89b0b2cff0faa89c89fefd3
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-09-14 16:54:49 -0300

    Criando o formulário de cadastro de galpões

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,19 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+  def new
+
+  end
+  def create
+    @warehouse = Warehouse.new(warehouse_params)
+    @warehouse.save()
+    redirect_to root_path 
+  end
+
+  private
+  
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :description, :address, :city, 
+                        :cep, :area)
+  end
 end
---
+++ spec/system/user_register_warehouse_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe "Usuário cadastra um galpão" do
+  it 'a partir da tela inicial' do
+    #arrange
+    #act
+    visit root_path
+    click_on 'Cadastrar Galpão'
+    #assert
+    expect(page).to have_field('Nome') 
+    expect(page).to have_field('Descrição')
+    expect(page).to have_field('Código')    
+    expect(page).to have_field('Endereço')
+    expect(page).to have_field('Cidade')
+    expect(page).to have_field('CEP')
+    expect(page).to have_field('Área')
+
+  end
+
+  it 'com sucesso' do
+    #act
+    visit root_path
+    click_on 'Cadastrar Galpão'
+    fill_in "Nome",	with: "Rio de Janeiro"
+    fill_in "Descrição",	with: "Galpão da área da zona portuária do Rio"  
+    fill_in "Código",	with: "RIO"  
+    fill_in "Endereço",	with: "Avenida do Museu do amanhã, 1000"  
+    fill_in "Cidade",	with: "Rio de Janeiro"  
+    fill_in "CEP",	with: "201000-000"  
+    fill_in "Área",	with: "3200"  
+    click_on 'Enviar'
+    #assert
+    expect(current_path).to  eq root_path 
+    expect(page).to have_content 'Rio de Janeiro' 
+    expect(page).to have_content 'Rio'
+    expect(page).to have_content '3200 m2' 
+
+  end
+end
+ 
