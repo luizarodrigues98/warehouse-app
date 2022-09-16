f3c3723ecbd15d3e77ccbbad7d91234fe53946ff
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-09-16 11:35:28 -0300

    Primeiros testes unitários

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,3 +1,5 @@
 class Warehouse < ApplicationRecord
   validates :name, :code, :city, :address, :cep, :description, presence: true
+  validates :code, uniqueness: true
 end
+
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,4 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
+  describe 'valid?' do
+    context "presence" do
+          
+      it 'false when name is empty' do
+        warehouse = Warehouse.new(name:'', code: 'Rio', address: 'Endereço',
+                                  cep:'25000-00', city: 'Rio', area: 1000, 
+                                description: 'Alguma descrição')
+        #act
+        result = warehouse.valid?
+        #assert
+        expect(result).to eq false 
+
+      end  
+      it 'false when code is empty' do
+        warehouse = Warehouse.new(name:'Rio', code: '', address: 'Endereço',
+                                  cep:'25000-00', city: 'Rio', area: 1000, 
+                                description: 'Alguma descrição')
+        #act
+        result = warehouse.valid?
+        #assert
+        expect(result).to eq false 
+
+      end  
+      it 'false when address is empty' do
+        warehouse = Warehouse.new(name:'Rio', code: 'Rio', address: '',
+                                  cep:'25000-00', city: 'Rio', area: 1000, 
+                                description: 'Alguma descrição')
+        #act
+        result = warehouse.valid?
+        #assert
+        expect(result).to eq false 
+
+      end  
+    end
+    
+    it 'false when code is already in use' do
+      #arrange: criar dois galpões e repetir o cdigo
+      first_warehouse = Warehouse.create(name:'Rio', code: 'Rio', address: 'Endereço',
+                                      cep:'25000-00', city: 'Rio', area: 1000, 
+                                      description: 'Alguma descrição')
+      second_warehouse =  Warehouse.new(name:'Niteroi', code: 'Rio', address: 'Avenida',
+                                        cep:'35000-00', city: 'Niteroi', area: 1500, 
+                                        description: 'Alguma descrição')                                
+      #act: perguntar pro segundo galpão se ele é valido, fiz essa etapa dentro do assert
+      #assert:: espero que seja false 
+      expect(second_warehouse.valid?).to be_falsey
+      #poderia usar também:  expect(second_warehouse.valid?).not_to be_valid
+    end
+  end
 end
