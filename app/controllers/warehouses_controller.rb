9dcdbaf2b2599522c7b09c3e5ce132800c2695ad
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-09-19 14:34:47 -0300

    Removendo galpões

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,5 +1,5 @@
 class WarehousesController < ApplicationController
-  before_action :set_warehouse, only: [:show, :edit, :update]
+  before_action :set_warehouse, only: [:show, :edit, :update, :destroy]
   def show
   end
 
@@ -29,6 +29,10 @@ class WarehousesController < ApplicationController
     end
   end
 
+  def destroy
+    @warehouse.destroy 
+    redirect_to root_path, notice: 'Galpão removido com sucesso'      
+  end
   private
   
    def set_warehouse
