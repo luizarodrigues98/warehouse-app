597a54b816ba0ea1e23de1528f67744637c9ccac
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-09-19 16:44:43 -0300

    Criando model e migration Supplier

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_14_143419) do
+ActiveRecord::Schema.define(version: 2022_09_19_194119) do
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "corporate_name"
+    t.string "brand_name"
+    t.string "registration_number"
+    t.string "full_address"
+    t.string "city"
+    t.string "state"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "warehouses", force: :cascade do |t|
     t.string "name"
