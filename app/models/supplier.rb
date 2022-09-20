75843f072ad21c0e8686d39c037a9e31b18ffd3e
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-09-20 12:03:51 -0300

    Validação do tamanho da string registration_number

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,5 @@
 class Supplier < ApplicationRecord
+  validates :corporate_name, :brand_name, :registration_number, :email, presence: true
+  validates :registration_number, uniqueness: true
+  validates_length_of :registration_number, is: 13
 end
