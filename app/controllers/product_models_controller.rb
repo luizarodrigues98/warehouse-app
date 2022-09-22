39a3c8f9109082b243dfc94da66bdcf399a63471
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-09-22 14:19:49 -0300

    Formularios com associações no product_models

--- app/controllers/product_models_controller.rb
+++ app/controllers/product_models_controller.rb
@@ -3,4 +3,24 @@ class ProductModelsController < ApplicationController
   def index
     @product_models = ProductModel.all
   end
+
+  def new 
+    @product_model = ProductModel.new    
+    @suppliers = Supplier.all
+  end
+
+  def create
+    @product_model = ProductModel.new(product_models_params) 
+    @product_model.save
+    redirect_to @product_models
+  end
+
+  def show
+    
+  end
+
+  private 
+    def product_models_params
+      params.require(:product_model).permit(:name, :weight, :height, :width, :depth, :sku, :supplier_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :warehouses, except: [:index]
   resources :suppliers
-  resources :product_models, only: [:index]
+  resources :product_models, only: [:index, :new, :create, :show]
 end
