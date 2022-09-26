8885302ed188314a163cf0748d33ab8cf97de22f
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-09-26 18:45:22 -0300

    Logout do user

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -12,7 +12,7 @@ describe "Usuário se autentica" do
     end
     within('nav') do
       expect(page).not_to  have_link 'Entrar' 
-      expect(page).to have_link 'Sair'
+      expect(page).to have_button 'Sair'
       expect(page).to have_content "joao@email.com"  
     end
   end
@@ -27,5 +27,20 @@ describe "Usuário se autentica" do
       click_on 'Entrar'
     end
     expect(page).to have_content "Login efetuado com sucesso."
+  end
+
+  it 'e faz logout' do
+    User.create!(email:"joao@email.com", password:"password")
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in "E-mail",	with: "joao@email.com" 
+      fill_in "Senha",	with: "password" 
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).to have_link 'Entrar' 
+    expect(page).not_to have_button 'Sair' 
   end
 end
